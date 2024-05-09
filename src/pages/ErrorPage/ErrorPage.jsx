5dee656d997e5f59244f95228e5452b2c5852295
Juwelmia63 <designghor5@gmail.com>
2024-05-09 23:18:48 +0600

    project setup complite for assignment ten

---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,10 @@
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h1 className="text-6xl font-bold">this is error page now i have to design it</h1>
+        </div>
+    );
+};
+
+export default ErrorPage;
