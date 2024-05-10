89f2c213bcf621ad820e6c66b1c5beb4b737a723
Juwelmia63 <designghor5@gmail.com>
2024-05-10 18:35:47 +0600

    footer added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navber from "../pages/sharedpage/Navber/Navber";
+import Footer from "../pages/sharedpage/Footer/Footer";
 
 const Main = () => {
     return (
         <div>
             <Navber></Navber>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
