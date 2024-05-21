d3970d7237557e08bc7a5fd0a83c0f97edd0ec1e
Juwelmia63 <designghor5@gmail.com>
2024-05-21 21:54:06 +0600

    gallery section banner added

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -1,8 +1,18 @@
+import { Link } from "react-router-dom";
+import { Typewriter } from "react-simple-typewriter";
 
 const Gallery = () => {
     return (
-        <div>
-            <h1>this is gallery section</h1>
+        <div className="hero min-h-96 my-5 object-cover" style={{ backgroundImage: 'url(https://i.ibb.co/NVXPFXX/360-F-306054576-c-ODtak-EEANIH8-BZN8i-LD8ltthqdr-MCGo.jpg)' }}>
+            <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-content text-center text-neutral-content">
+                <div className="max-w-md text-white bg-[#EA6A12] p-4 rounded-xl">
+                    <h1 className="mb-2 text-4xl font-bold font-playfairDisplay text-white">FOOD GALLERY
+                      </h1>
+                    <p className="mb-2 mt-0 w-full font-poppins text-xl font-bold text-black font-playfairDisplay">Gallery</p>
+                   
+                </div>
+            </div>
         </div>
     );
 };
